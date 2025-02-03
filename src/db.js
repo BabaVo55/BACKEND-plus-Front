65aaec6d81f93bcffa7671e371ec8bdd0792078d
BabaVo55 <ethemcode@outlook.com>
2025-02-03 16:35:27 +0000

    pretty far ahead but owwel

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import {DatabaseSync} from 'node:sqlite';
+const db = new DatabaseSync(':memory:');
+
+// Execute SQL statements from strings
+db.exec(`
+        CREATE TABLE user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE,
+            password TEXT
+        )
+    `)
+
+    db.exec(`
+            CREATE TABLE todos (
+                id INTEGER,
+                user_id INTEGER,
+                
+            )
+        `)
---
+++ src/server.js
@@ -0,0 +1,33 @@
+// const express = require('express');
+import express from 'express';
+import path, {dirname} from 'path';
+import {fileURLToPath} from 'url';
+const app = express();
+
+
+const PORT = process.env.PORT || 8080;
+
+// Get the file path from the URL of the current module
+const __filename = fileURLToPath(import.meta.url);
+// Get the directory name from the file path
+const __dirname = dirname(__filename)
+
+//Middleware:
+app.use(express.json());
+
+// Serves the HTML file from public not src.
+// Also will tell EXPRESS to serve all file from the public folder as static assets / 
+    // files. Any requests for the CSS files will be resolve ito the public directory.
+app.use(express.static(path.join(__dirname, '../public')))
+
+// Serving up the HTML from file 
+app.get('/', (req, res) => {
+
+    // If we only use the CODE bellow all CSS is out the window.
+    // res.sendFile(path.join(__dirname, '../public', 'index.html'))
+
+    res.sendFile(path.join(__dirname, '/public', 'index.html'))
+    
+})
+console.log('hellow wordl')
+app.listen(PORT, () => console.log('8080 plugged in'))
